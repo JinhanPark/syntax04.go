458e39a611b9476db7163dbbbfd6fd8072d75a3a
inhaJinhanPark <JinhanPark>
2021-09-13 21:21:33 +0900

    week3 version03

--- syntax04.go
+++ syntax04.go
@@ -13,5 +13,10 @@ func main(){
 	isPrime := true
 	number := rand.Intn(150) + 2
 	fmt.Println("임의로 추출된 수 : ", number)
-
+	if isPrime { //비교 연산자 제거}
+		fmt.Println(number, "는(은) 소수입니다!")
+	
+	} else {
+		fmt.Println(number, "는(은) 소수가 아닙니다~")
+	}
 }
